2b3ee1cff2f4cae50e13a708c5bf90e314f7f5b9
Paul <47947919+paul0665@users.noreply.github.com>
2020-03-17 16:34:08 +0800

    Add files via upload

---
+++ src/main/java/com/serverworld/worldSocket/paperspigot/util/messager.java
@@ -0,0 +1,16 @@
+package com.serverworld.worldSocket.paperspigot.util;
+
+import com.serverworld.worldSocket.paperspigot.worldSocket;
+import com.serverworld.worldSocket.paperspigot.socket.socketclient;
+
+public class messager {
+
+    static worldSocket worldsocket;
+    public messager(worldSocket worldSocket){
+        this.worldsocket = worldSocket;
+    }
+
+    public static void sendmessage (String message){
+        worldsocket.client.sendmessage(message);
+    }
+}
